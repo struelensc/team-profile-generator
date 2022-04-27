830f5a822fddf5add92a1385ea8e1c1a5e38d2fa
Courtney Struelens <courtneystruelens@hotmail.com>
2022-04-26 21:52:47 -0700

    set up engineer class

---
+++ tests/Engineer.test.js
@@ -0,0 +1,20 @@
+const Engineer = require("../lib/Engineer");
+
+describe("Employee class", () => {
+  it("should return the employee github username when called", () => {
+    const name = "John";
+    const id = 36;
+    const email = "abc@email.com";
+    const github = "coolperson";
+
+    const newEmployee = new Engineer(name, id, email, github);
+
+    expect(newEmployee.getGithub()).toBe("coolperson");
+  });
+
+  it("should return the employee role when called", () => {
+    const newEmployee = new Engineer("John");
+
+    expect(newEmployee.getRole()).toBe("Engineer");
+  });
+});
