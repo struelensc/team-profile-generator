8f9b5c21745a3d1247cbff80cbbf23464b43752b
Courtney Struelens <courtneystruelens@hotmail.com>
2022-04-26 21:14:28 -0700

    Added script in package.json and employee class

---
+++ tests/Employee.test.js
@@ -0,0 +1,31 @@
+const Employee = require("../lib/Employee");
+
+describe("Employee class", () => {
+  it("should return the employee name when called", () => {
+    const name = "John";
+
+    const newEmployee = new Employee(name);
+
+    expect(newEmployee.getName()).toBe("John");
+  });
+
+  it("should return the employee id when called", () => {
+    const id = 36;
+
+    const newEmployee = new Employee("John", id);
+
+    expect(newEmployee.getId()).toBe(36);
+  });
+
+  it("should return the employee email when called", () => {
+    const email = "abc@email.com";
+
+    const newEmployee = new Employee("John", 36, email);
+
+    expect(newEmployee.getEmail()).toBe("abc@email.com");
+  });
+
+  it("should return the employee role when called", () => {
+    expect(newEmployee.getRole().toBe("Employee"));
+  });
+});
